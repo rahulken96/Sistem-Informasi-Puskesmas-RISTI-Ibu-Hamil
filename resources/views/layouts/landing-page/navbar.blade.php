420f43b0c0cb9cca1fe1b8fd5347a35a50df2f9e
isaias0412 <muhammadisaias@gmail.com>
2022-04-07 05:44:46 +0700

    [Update] Landing Page

---
+++ resources/views/layouts/landing-page/navbar.blade.php
@@ -0,0 +1,15 @@
+<nav id="navbar" class="navbar">
+    <ul>
+      
+      <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
+      <li class="dropdown"><a href="#">Login Sebagai <i class="bi bi-chevron-down"></i></a>
+        <ul>
+          <li><a href="login.html">Admin</a></li>
+          <li><a href="login.html">Ibu Bidan</a></li>
+          <li><a href="#">Kepala Desa</a></li>
+          <li><a href="#">Kepala Puskesmas</a></li>
+        </ul>
+      </li>
+    </ul>
+    <i class="bi bi-list mobile-nav-toggle"></i>
+  </nav><!-- .navbar -->
