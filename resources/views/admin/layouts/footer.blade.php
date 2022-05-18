9a62728a43ecc6ccd1ae0e63ea5cc371f2d1f512
isaias0412 <muhammadisaias@gmail.com>
2022-05-18 21:49:28 +0700

    rapihin admin

---
+++ resources/views/admin/layouts/footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="footer">
+    <div class="container-fluid">
+
+        <div class="copyright ml-auto">
+            2021, made with <i class="fa fa-heart heart text-danger"></i> Polindra </a>
+        </div>
+    </div>
+</footer>
---
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -0,0 +1,61 @@
+<div class="sidebar sidebar-style-2">
+    <div class="sidebar-wrapper scrollbar scrollbar-inner">
+        <div class="sidebar-content">
+            <div class="user">
+                <div class="avatar-sm float-left mr-2">
+                    <img src="../assets/img/profile.jpg" alt="..." class="avatar-img rounded-circle">
+                </div>
+                <div class="info">
+                    <a data-toggle="collapse" href="#collapseExample" aria-expanded="true">
+                        <span>
+                            Admin
+                            <span class="user-level">Administrator</span>
+                            <!-- <span class="caret"></span> -->
+                        </span>
+                    </a>
+                    <div class="clearfix"></div>
+x
+                    <div class="collapse in" id="collapseExample">
+                        <!-- <ul class="nav">
+                            <li>
+                                <a href="#profile">
+                                    <span class="link-collapse">My Profile</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#edit">
+                                    <span class="link-collapse">Edit Profile</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#settings">
+                                    <span class="link-collapse">Settings</span>
+                                </a>
+                            </li>
+                        </ul> -->
+                    </div>
+                </div>
+            </div>
+            <ul class="nav nav">
+                <li class="nav-item active">
+                    <a data-toggle="collapse" href="index.html" class="collapsed" aria-expanded="false">
+                        <i class="fas fa-home"></i>
+                        <p>Dashboard</p>
+                        <!-- <span class="caret"></span> -->
+                        <a class="collapse" href="data-pasien.blade.php" class="collapsed" aria-expanded="false">
+                            <i class="fa fa-user "></i>
+                            <p>Data Pasien</p>
+                            <!-- <span class="caret"></span> -->
+                        </a>
+                        <a clas="collapse" href="cetak-laporan.html" class="collapsed" aria-expanded="false">
+                            <i class="fa fa-print fa-lg box-icon"></i>
+                            <p>Cetak Laporan</p>
+                            <!-- <span class="caret"></span> -->
+                        </a>
+
+                    </a>
+            </ul>
+        </div>
+    </div>
+</div>
+<!-- End Sidebar -->
