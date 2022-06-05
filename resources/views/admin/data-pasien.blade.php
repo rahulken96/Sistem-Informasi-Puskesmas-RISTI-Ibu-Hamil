148e71afd11b800d47e48aa8b53e1c216715b891
Rahul Ken Y <rahulken96@gmail.com>
2022-06-05 20:54:42 +0700

    [Progress] Admin

--- resources/views/admin/data-pasien.blade.php
+++ resources/views/admin/data-pasien.blade.php
@@ -29,6 +29,9 @@
 			<!-- Logo Header -->
 			<div class="logo-header" data-background-color="blue">
 
+				<!-- <a href="index.html" class="logo">
+					<img src="../assets/img/logo.svg" alt="navbar brand" class="navbar-brand">
+				</a> -->
 				<button class="navbar-toggler sidenav-toggler ml-auto" type="button" data-toggle="collapse" data-target="collapse" aria-expanded="false" aria-label="Toggle navigation">
 					<span class="navbar-toggler-icon">
 						<i class="icon-menu"></i>
@@ -103,7 +106,6 @@
 								</span>
 							</a>
 							<div class="clearfix"></div>
-x
 							<div class="collapse in" id="collapseExample">
 
 							</div>
